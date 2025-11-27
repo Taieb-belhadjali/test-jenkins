00d40465fd131fcee68427778d32cb88259f6d1c
Taieb-belhadjali <Taiebamine.Belhadjali@esprit.tn>
2025-11-27 11:15:32 +0100

    changes1

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -3,11 +3,22 @@ package tn.esprit.studentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
-class StudentManagementApplicationTests {
+class CalculatorServiceTest {
 
     @Test
-    void contextLoads() {
+    void testAdd() {
+        CalculatorServiceTest c = new CalculatorServiceTest();
+        assertEquals(5, c.add(2,3));
+    }
+
+    private void assertEquals(int i, int add) {
+        //
+    }
+
+    private int add(int i, int i1) {
+        return i + i1;
     }
 
 }
